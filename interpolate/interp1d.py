762c0e7938df82f3365ed9d6105cfb1e431bc220
Antoine Liutkus <antoine.liutkus@inria.fr>
2018-08-24 08:29:38 +0200

    added the support for an output tensor to avoid allocating each time

--- interpolate/interp1d.py
+++ interpolate/interp1d.py
@@ -1,21 +1,20 @@
 import torch
 try:
-    from .searchsorted import searchsorted
+    from pytorch_searchsorted.searchsorted import searchsorted
 except ImportError:
     raise ImportError(
      '\nThe CUDA interp1d function depends on the '
      'pytorch-searchsorted module.\n'
      'You must:\n'
      ' * clone this repo through '
-     '`git clone git@github.com:aliutkus/pytorch-searchsorted.git`.\n'
+     '`git clone git@github.com:aliutkus/pytorch_searchsorted.git`.\n'
      ' * Go in the corresponding directory and launch `make`.\n'
-     ' * Copy the whole `searchsorted` directory that has been generated '
-     'as a subfolder of the present `interpolate` folder.\n')
+     ' * Have this pytorch_searchsorted directory in your Python path\n')
 
 import scipy.interpolate as sp_interp
 
 
-def interp1d(x, y, xnew, cuda=True):
+def interp1d(x, y, xnew, out=None, cuda=True):
     """
     Linear 1D interpolation on the GPU for Pytorch.
     This function returns interpolated values of a set of 1-D functions at
@@ -35,6 +34,8 @@ def interp1d(x, y, xnew, cuda=True):
         A 1-D or 2-D tensor of real values. `xnew` can only be 1-D if _both_
         `x` and `y` are 1-D. Otherwise, its length along the first dimension
         must be the same as that of whichever `x` and `y` is 2-D.
+    out : Pytorch Tensor, same shape as `xnew`
+        Tensor for the output. If None: allocated automatically.
     cuda : bool
         if True, will attempt to use CUDA if available.
         If False, a non-optimized loop over D will take place, calling the
@@ -67,19 +68,32 @@ def interp1d(x, y, xnew, cuda=True):
             ), ("x and y must have the same number of columns, and either "
                 "the same number of row or one of them having only one row.")
 
-    # starting with handling the cpu version through scipy
-    if not cuda or not torch.cuda.is_available():
-        reshaped_xnew = False
-        if ((v['x'].shape[0] == 1) and (v['y'].shape[0] == 1)
-           and (v['xnew'].shape[0] > 1)):
-            # if there is only one row for both x and y, there is no need to
-            # loop over the rows of xnew because they will all have to face the
-            # same interpolation problem. We should just stack them together to
-            # call interp1d and put them back in place afterwards.
-            original_xnew_shape = v['xnew'].shape
-            v['xnew'] = v['xnew'].view(1, -1)
-            reshaped_xnew = True
+    reshaped_xnew = False
+    if ((v['x'].shape[0] == 1) and (v['y'].shape[0] == 1)
+       and (v['xnew'].shape[0] > 1)):
+        # if there is only one row for both x and y, there is no need to
+        # loop over the rows of xnew because they will all have to face the
+        # same interpolation problem. We should just stack them together to
+        # call interp1d and put them back in place afterwards.
+        original_xnew_shape = v['xnew'].shape
+        v['xnew'] = v['xnew'].view(1, -1)
+        reshaped_xnew = True
 
+    # identifying the device to use
+    device = 'cpu' if not cuda or not torch.cuda.is_available() else 'cuda'
+
+    # identify the dimensions of output and check if the one provided is ok
+    D = max(v['x'].shape[0], v['xnew'].shape[0])
+    shape_ynew = (D, v['xnew'].shape[-1])
+    if out is not None:
+        assert out.numel() == shape_ynew[0]*shape_ynew[1], (
+                'The output provided is of incorrect shape.')
+        ynew = out.reshape(shape_ynew)
+    else:
+        ynew = torch.zeros(*shape_ynew, device=device)
+
+    # starting with handling the cpu version through scipy
+    if device == 'cpu':
         # helper function to get the unique row in case of 1-D inputs
         def get_row(name, d):
             if v[name].shape[0] == 1:
@@ -87,20 +101,17 @@ def interp1d(x, y, xnew, cuda=True):
             else:
                 return v[name][d].cpu().numpy()
 
-        # identify the dimensions of output
-        D = max(v['x'].shape[0], v['xnew'].shape[0])
-        yq = torch.zeros(D, v['xnew'].shape[-1], device='cpu')
-
         # for each dimension, apply interpolation
         for d in range(D):
             interp_func = sp_interp.interp1d(get_row('x', d),
                                              get_row('y', d),
                                              bounds_error=False,
                                              fill_value='extrapolate')
-            yq[d] = torch.tensor(interp_func(get_row('xnew', d)), device='cpu')
+            ynew[d] = torch.tensor(interp_func(get_row('xnew', d)),
+                                   device='cpu')
         if reshaped_xnew:
-            yq = yq.view(original_xnew_shape)
-        return yq
+            ynew = ynew.view(original_xnew_shape)
+        return ynew
 
     # the CPU case is handled if needs be. now we switch to the GPU case.
     # moving everything to GPU in case it was not there already (not handling
@@ -115,10 +126,12 @@ def interp1d(x, y, xnew, cuda=True):
     num_slopes = v['slopes'].shape[1]
 
     # calling searchsorted on the x values.
+    ind = ynew
+    searchsorted(v['x'], v['xnew'], ind)
     # the `-1` is because searchsorted looks for the index where the values
     # must be inserted to preserve order. And we want the index of the
     # preceeding value.
-    ind = searchsorted(v['x'], v['xnew']) - 1
+    ind -= 1
     ind = torch.clamp(ind, 0, num_slopes - 1).long()
 
     # helper function to select stuff according to the found indices.
@@ -128,5 +141,8 @@ def interp1d(x, y, xnew, cuda=True):
         return torch.gather(v[name], 1, ind)
 
     # now build the linear interpolation
-    res = sel('y') + sel('slopes')*(v['xnew'] - sel('x'))
-    return res
+    ynew = sel('y') + sel('slopes')*(v['xnew'] - sel('x'))
+    if reshaped_xnew:
+        ynew = ynew.view(original_xnew_shape)
+
+    return ynew
--- test.py
+++ test.py
@@ -6,46 +6,53 @@ from interpolate import interp1d
 
 
 if __name__ == "__main__":
+    # defining the number of tests
+    ntests = 2
+
     # problem dimensions
-    D = 100000
+    D = 1000
     Dnew = 1
     N = 100
     P = 30
 
-    # draw the data
-    x = torch.rand(D, N, device='cuda') * 10000
-    x = x.sort(dim=1)[0]
+    yq_gpu = None
+    yq_cpu = None
+    for ntest in range(ntests):
+        # draw the data
+        x = torch.rand(D, N, device='cuda') * 10000
+        x = x.sort(dim=1)[0]
 
-    y = torch.linspace(0, 1000, D*N).to('cuda').view(D, -1)
-    y -= y[:, 0, None]
+        y = torch.linspace(0, 1000, D*N).to('cuda').view(D, -1)
+        y -= y[:, 0, None]
 
-    xnew = torch.rand(Dnew, P, device='cuda')*10000
+        xnew = torch.rand(Dnew, P, device='cuda')*10000
 
-    print('Solving %d interpolation problems: '
-          'each with %d observations and %d desired values' % (D, N, P))
+        print('Solving %d interpolation problems: '
+              'each with %d observations and %d desired values' % (D, N, P))
 
-    # launching the cuda version
-    t0 = time.time()
-    yq_gpu = interp1d(x, y, xnew, cuda=True)
-    t1 = time.time()
+        # launching the cuda version
+        t0 = time.time()
+        yq_gpu = interp1d(x, y, xnew, yq_gpu, cuda=True)
+        t1 = time.time()
 
-    # putting everything back to CPU before calling the CPU version
-    x = x.to('cpu')
-    y = y.to('cpu')
-    xnew = xnew.to('cpu')
+        # putting everything back to CPU before calling the CPU version
+        x = x.to('cpu')
+        y = y.to('cpu')
+        xnew = xnew.to('cpu')
 
-    # calling the cpu version
-    t0_cpu = time.time()
-    yq_cpu = interp1d(x, y, xnew, cuda=False)
-    t1_cpu = time.time()
+        # calling the cpu version
+        t0_cpu = time.time()
+        yq_cpu = interp1d(x, y, xnew, yq_cpu, cuda=False)
+        t1_cpu = time.time()
 
-    # compute the difference between both
-    error = torch.norm(yq_cpu - yq_gpu.to('cpu'))/torch.norm(yq_cpu)*100.
-    print('CPU: %0.3fms, GPU: %0.3fms, error: %f%%.' % ((t1_cpu-t0_cpu)*1000,
-                                                        (t1-t0)*1000,
-                                                        error))
+        # compute the difference between both
+        error = torch.norm(yq_cpu - yq_gpu.to('cpu'))/torch.norm(yq_cpu)*100.
+        print('CPU: %0.3fms, '
+              'GPU: %0.3fms, error: %f%%.' % ((t1_cpu-t0_cpu)*1000,
+                                              (t1-t0)*1000,
+                                              error))
 
-    # plot the result for the first 10 dimensions at most
+    # for the last test, plot the result for the first 10 dimensions at most
     d_plot = min(D, 10)
     x = x[:d_plot].cpu().numpy()
     y = y[:d_plot].cpu().numpy()
