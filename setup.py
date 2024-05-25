9c81ade9c9d0f81c3ba039a9cfa91329d1d1055a
Antoine Liutkus <antoine.liutkus@inria.fr>
2024-05-24 20:20:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     name='torchinterp1d',
     version='1.1',
     description='An interp1d implementation for pytorch',
+    download_url='https://github.com/aliutkus/torchinterp1d/archive/refs/tags/v1.tar.gz',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Antoine Liutkus',
